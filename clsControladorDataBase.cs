01ce46a939e0caae5844f1f6b6916346d10dad56
Milagros <milagroschaconies@gmail.com>
2023-11-14 12:06:49 -0300

    descripción del código para clsControladorDataBase

--- clsControladorDataBase.cs
+++ clsControladorDataBase.cs
@@ -13,83 +13,99 @@ namespace pryMilagrosChaconIE
 {
     internal class clsControladorDataBase
     {
+        // Definición de variables de conexión y adaptador
         OleDbConnection conexionBD = new OleDbConnection();
         OleDbCommand comandoBD = new OleDbCommand();
         OleDbDataReader lectorBD;
         OleDbDataAdapter adaptadorBD;
 
+        // DataSet para almacenar datos
         DataSet objDataSet = new DataSet();
 
+        // Ruta y estado de conexión
         string rutaBD;
         public string estadoConexion = "";
+
+        // Variable estática para indicar acceso
         public static bool acceso;
+
+        // Cadena de conexión a la base de datos
         string cadenaConexionBD = @"Provider = Microsoft.ACE.OLEDB.12.0;" + " Data Source = ..\\..\\Resources\\bdUsers.accdb";
         string cadenaConexionELCLUB = @"Provider = Microsoft.ACE.OLEDB.12.0;" + " Data Source = ..\\..\\Resources\\EL_CLUB.accdb";
-
 
         public clsControladorDataBase()
         {
             try
             {
+                // Configura la conexión a la base de datos EL_CLUB en el constructor
                 conexionBD = new OleDbConnection();
+                comandoBD = new OleDbCommand();
                 conexionBD.ConnectionString = cadenaConexionELCLUB;
                 conexionBD.Open();
 
+                // Inicializa el DataSet
                 objDataSet = new DataSet();
 
+                // Establece el estado de conexión como "Conectado"
                 estadoConexion = "Conectado";
             }
             catch (Exception EX)
             {
+                // En caso de error, registra el mensaje de error
                 estadoConexion = EX.Message;
             }
-
-            conexionBD = new OleDbConnection();
-            comandoBD = new OleDbCommand();
         }
 
-        public void ConectarBD()
+        public void ConectarBD() // Método para conectar a la base de datos bdUsers
         {
             try
             {
+                // Configura la cadena de conexión y abre la conexión
                 conexionBD.ConnectionString = cadenaConexionBD;
                 conexionBD.Open();
             }
             catch (Exception EX)
             {
+                // En caso de error, muestra un MessageBox con la información de la excepción
                 MessageBox.Show(Convert.ToString(EX));
             }
         }
 
-        public void ConectarBDElClub()
+        public void ConectarBDElClub() // Método para conectar a la base de datos EL_CLUB
         {
             try
             {
+                // Configura la conexión a la base de datos EL_CLUB
                 conexionBD = new OleDbConnection();
                 conexionBD.ConnectionString = cadenaConexionELCLUB;
                 conexionBD.Open();
+
+                // Establece el estado de conexión como "Conectado"
                 estadoConexion = "Conectado";
             }
             catch (Exception EX)
             {
+                // En caso de error, establece el estado de conexión con un mensaje de error
                 estadoConexion = "Error:" + EX.Message;
             }
         }
 
-        public void Traer_Datos(DataGridView grilla)
+        public void Traer_Datos(DataGridView grilla) // Método para traer datos y mostrarlos en un DataGridView
         {
             string estadoCliente = "";
 
             try
             {
+                // Configura el comando para trabajar con la tabla "SOCIOS"
                 comandoBD = new OleDbCommand();
-
                 comandoBD.Connection = conexionBD;
                 comandoBD.CommandType = System.Data.CommandType.TableDirect;
                 comandoBD.CommandText = "SOCIOS";
 
+                // Ejecuta el comando y obtiene un lector de datos
                 lectorBD = comandoBD.ExecuteReader();
 
+                // Agregado de columnas al DataGridView
                 grilla.Columns.Add("ID", "ID");
                 grilla.Columns.Add("Nombre", "Nombre");
                 grilla.Columns.Add("Apellido", "Apellido");
@@ -100,33 +116,31 @@ namespace pryMilagrosChaconIE
                 grilla.Columns.Add("Puntaje", "Puntaje");
                 grilla.Columns.Add("Estado", "Estado");
 
+                // Llena el DataGridView con los datos del lector
                 if (lectorBD.HasRows)
                 {
                     while (lectorBD.Read())
                     {
-                        if (lectorBD.GetBoolean(8) == true)
-                        {
-                            estadoCliente = "Activo";
-                        }
-                        else
-                        {
-                            estadoCliente = "Inactivo";
-                        }
+                        // Determina el estado del cliente
+                        estadoCliente = lectorBD.GetBoolean(8) ? "Activo" : "Inactivo";
 
+                        // Agrega una nueva fila al DataGridView
                         grilla.Rows.Add(lectorBD[0], lectorBD[1], lectorBD[2], lectorBD[3], lectorBD[4], lectorBD[6], lectorBD[7], estadoCliente);
-
                     }
                 }
             }
             catch (Exception EX)
             {
+                // En caso de error, establece el estado de conexión con un mensaje de error
                 estadoConexion = "Error:" + EX.Message;
             }
         }
-        public void BuscarUsuario(int Usuario)
+
+        public void BuscarUsuario(int Usuario) // Método para buscar un usuario por ID
         {
             OleDbDataReader lector;
             lector = comandoBD.ExecuteReader();
+
             if (lector.HasRows)
             {
                 bool existe = false;
@@ -134,6 +148,7 @@ namespace pryMilagrosChaconIE
                 {
                     if (int.Parse(lector[0].ToString()) == Usuario)
                     {
+                        // Muestra un MessageBox indicando que el cliente existe
                         MessageBox.Show("Cliente Existe",
                             "Consulta",
                             MessageBoxButtons.OK,
@@ -142,8 +157,9 @@ namespace pryMilagrosChaconIE
                         break;
                     }
                 }
-                if (existe == false)
+                if (!existe)
                 {
+                    // Muestra un MessageBox indicando que el cliente no existe
                     MessageBox.Show("Este cliente No Existe",
                             "Consulta",
                             MessageBoxButtons.OK,
@@ -156,14 +172,18 @@ namespace pryMilagrosChaconIE
         public Boolean BuscarContraseña(string Usuario, string Contraseña)
         {
             Boolean coincide = false;
+
             try
             {
+                // Ejecuta el lector de datos
                 OleDbDataReader lector;
                 lector = comandoBD.ExecuteReader();
+
                 if (lector.HasRows)
                 {
                     while (lector.Read())
                     {
+                        // Verifica si el usuario y la contraseña coinciden
                         if (lector[1].ToString() == Usuario && lector[2].ToString() == Contraseña)
                         {
                             coincide = true;
@@ -172,6 +192,7 @@ namespace pryMilagrosChaconIE
                         }
                     }
                 }
+
                 lector.Close();
                 return coincide;
             }
@@ -190,25 +211,31 @@ namespace pryMilagrosChaconIE
 
             try
             {
+                // Configuración de la conexión a la base de datos EL_CLUB
                 conexionBD = new OleDbConnection();
                 conexionBD.ConnectionString = cadenaConexionELCLUB;
                 conexionBD.Open();
 
+                // Se establece el estado de conexión como "Conectado"
                 estadoConexion = "Conectado";
 
             }
             catch (Exception EX)
             {
+                // En caso de error, se establece el estado de conexión con un mensaje de error
                 estadoConexion = "Error" + EX.Message;
             }
 
+            // Configura el comando para trabajar con la tabla "SOCIO"
             comandoBD.Connection = conexionBD;
             comandoBD.CommandType = CommandType.TableDirect;
             comandoBD.CommandText = "SOCIO";
 
+            // Configura el adaptador y llena el DataSet con los datos de la tabla "SOCIO"
             objAdaptador = new OleDbDataAdapter(comandoBD);
             objAdaptador.Fill(objDataSet, "SOCIO");
 
+            // Obtiene la tabla "SOCIO" del DataSet
             DataTable dt = objDataSet.Tables["SOCIO"];
 
             foreach (DataRow registro in dt.Rows)
@@ -217,6 +244,7 @@ namespace pryMilagrosChaconIE
                 {
                     registro.BeginEdit();
 
+                    // Cambia el estado del socio
                     if ((bool)registro["ESTADO"])
                     {
                         registro["ESTADO"] = false;
@@ -232,10 +260,13 @@ namespace pryMilagrosChaconIE
                 }
             }
 
+            // Constructor de comandos para el adaptador
             OleDbCommandBuilder constructor = new OleDbCommandBuilder(objAdaptador);
 
+            // Actualización de la base de datos con el cambio de estado
             objAdaptador.Update(objDataSet, "SOCIOS");
 
+            // Muestra un MessageBox indicando que el cambio de estado fue exitoso
             MessageBox.Show("Cambio de estado exitoso");
         }
     }
